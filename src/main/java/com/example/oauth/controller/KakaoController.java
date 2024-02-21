4a76b4f49bc7ef54b404e14d020b34c183b51180
HyeonJin Song <shwj1602@naver.com>
2024-02-21 14:23:24 +0900

    Feat : 카카오 인증 서버로부터 엑세스 토큰으로 사용자 정보 불러오기 추가

--- src/main/java/com/example/oauth/controller/KakaoController.java
+++ src/main/java/com/example/oauth/controller/KakaoController.java
@@ -1,9 +1,24 @@
 package com.example.oauth.controller;
 
+import com.example.oauth.dto.KakaoDto;
+import com.example.oauth.entity.MsgEntity;
+import com.example.oauth.service.KakaoService;
+import jakarta.servlet.http.HttpServletRequest;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequiredArgsConstructor
 public class KakaoController {
+    private final KakaoService kakaoService;
+
+    @GetMapping("/user/oauth")
+    public ResponseEntity<MsgEntity> callback(HttpServletRequest request) throws Exception{
+        KakaoDto kakaoInfo = kakaoService.getKakaoInfo(request.getParameter("code"));
+
+        return ResponseEntity.ok()
+                .body(new MsgEntity("Success", kakaoInfo));
+    }
 }
--- src/main/java/com/example/oauth/service/KakaoService.java
+++ src/main/java/com/example/oauth/service/KakaoService.java
@@ -1,7 +1,17 @@
 package com.example.oauth.service;
 
+import com.example.oauth.dto.KakaoDto;
+import org.json.simple.JSONObject;
+import org.json.simple.parser.JSONParser;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
+import org.springframework.util.LinkedMultiValueMap;
+import org.springframework.util.MultiValueMap;
+import org.springframework.web.client.RestTemplate;
 
 @Service
 public class KakaoService {
@@ -20,6 +30,70 @@ public class KakaoService {
                 +"?client_id="+KAKAO_ID
                 +"&redirect_uri="+KAKAO_REDIRECT_URI
                 +"&response_type=code";
+    }
+
+    public KakaoDto getKakaoInfo(String code) throws Exception{
+        if(code == null) throw new Exception("Failed get authorization code");
+
+        String accessToken = "";
+
+        try {
+            HttpHeaders headers = new HttpHeaders();
+            headers.add("Content-type", "application/x-www-form-urlencoded");
+
+            MultiValueMap<String, String> params = new LinkedMultiValueMap<>();
+            params.add("grant_type", "authorization_code");
+            params.add("client_id", KAKAO_ID);
+            params.add("client_secret", KAKAO_SECRET);
+            params.add("code", code);
+            params.add("redirect_uri" , KAKAO_REDIRECT_URI);
+
+            RestTemplate restTemplate = new RestTemplate();
+            HttpEntity<MultiValueMap<String, String>> httpEntity = new HttpEntity<>(params, headers);
+
+            ResponseEntity<String> response = restTemplate.exchange(
+                    KAKAO_AUTH_URI + "/oauth/token",
+                    HttpMethod.POST,
+                    httpEntity,
+                    String.class
+            );
+
+            JSONParser jsonParser = new JSONParser();
+            JSONObject jsonObj = (JSONObject) jsonParser.parse(response.getBody());
+
+            accessToken = (String) jsonObj.get("access_token");
+        } catch (Exception e){
+            throw new Exception("API call failed");
+        }
+        return getUserWithToken(accessToken);
+    }
+
+    private KakaoDto getUserWithToken(String accessToken) throws Exception{
+        HttpHeaders headers = new HttpHeaders();
+        headers.add("Authorization", "Bearer "+accessToken);
+        headers.add("Content-type", "application/x-www-form-urlencoded;charset=utf-8");
+
+        RestTemplate restTemplate = new RestTemplate();
+        HttpEntity<MultiValueMap<String, String>> httpEntity = new HttpEntity<>(headers);
+        ResponseEntity<String> response = restTemplate.exchange(
+                KAKAO_API_URI + "/v2/user/me",
+                HttpMethod.POST,
+                httpEntity,
+                String.class
+        );
+
+        JSONParser jsonParser = new JSONParser();
+        JSONObject jsonObj = (JSONObject) jsonParser.parse(response.getBody());
+        JSONObject account = (JSONObject) jsonObj.get("kakao_account");
+        JSONObject profile = (JSONObject) account.get("profile");
+
+        long id = (long) jsonObj.get("id");
+        String nickname = String.valueOf(profile.get("nickname"));
+
+        return KakaoDto.builder()
+                .id(id)
+                .nickname(nickname)
+                .build();
     }
 
 }
